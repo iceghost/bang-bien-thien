b4eec6c69684b1477ef1e61da33fa510ef7f7dc6
Duy Khang <khangemail22@gmail.com>
2020-02-04 16:06:17 +0700

    Hello world!

---
+++ src/Main.elm
@@ -0,0 +1,45 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+ 
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type alias Model =
+    { property : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "hehe", Cmd.none )
+
+
+type Msg
+    = Msg1
+    | Msg2
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Msg1 ->
+            ( model, Cmd.none )
+
+        Msg2 ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Hello world!" ]
